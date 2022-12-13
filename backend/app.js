6341c0d5db7f302881ed5b278aaa94daad3ce1df
Deeptiranjan Swain <87275698+DeeptiranjanSwain79@users.noreply.github.com>
2022-12-13 18:45:49 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-    res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "/frontend/index.html"));
 })
 
 //middleware for error
